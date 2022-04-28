e9d5f03879be97276823ac6f43f336ba1544f1ae
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-28 06:49:27 +0530

    Update terraform/tf-module-mongodb/

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     bucket = "terraform-b63"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
