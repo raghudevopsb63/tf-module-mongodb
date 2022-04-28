830e06272b852664bb4f725d33ad9038b38ab00b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-28 06:46:17 +0530

    Update terraform/tf-module-mongodb/

--- cluster.tf
+++ cluster.tf
@@ -1,15 +1,15 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier = "roboshop-${var.ENV}"
-  engine             = "docdb"
-  master_username    = "admin1"
-  master_password    = "roboshop1"
-  ## This is just for lab purpose
-  skip_final_snapshot = true
-}
+//resource "aws_docdb_cluster" "docdb" {
+//  cluster_identifier = "roboshop-${var.ENV}"
+//  engine             = "docdb"
+//  master_username    = "admin1"
+//  master_password    = "roboshop1"
+//  ## This is just for lab purpose
+//  skip_final_snapshot = true
+//}
 
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-${var.ENV}"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
   tags = {
     Name = "roboshop-${var.ENV}"
