91a9948e7844086d92bf060073f973bc4c74dc72
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-28 06:56:13 +0530

    Update terraform/tf-module-mongodb/

--- cluster.tf
+++ cluster.tf
@@ -17,3 +17,9 @@ resource "aws_docdb_subnet_group" "docdb" {
   }
 }
 
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "roboshop-${var.ENV}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
+}
