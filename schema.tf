96319f3bf304289458ffef2368fa19698daa28ff
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-29 07:25:28 +0530

    Update terraform/tf-module-mongodb/

--- schema.tf
+++ schema.tf
@@ -5,8 +5,8 @@ cd /tmp
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 unzip -o mongodb.zip
 cd mongodb-main
-mongo --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < catalogue.js
-mongo --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < users.js
+mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < catalogue.js
+mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host ${aws_docdb_cluster.docdb.endpoint} --username admin1 --password roboshop1 < users.js
 EOF
   }
 }
