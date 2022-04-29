0072c3db3d2468dd1af35cdb6a27b6cb9090c6d3
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-29 07:21:10 +0530

    Update terraform/tf-module-mongodb/

--- cluster.tf
+++ cluster.tf
@@ -23,3 +23,29 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = "db.t3.medium"
 }
+
+resource "aws_security_group" "allow_mongodb" {
+  name        = "roboshop-mongodb-${var.ENV}"
+  description = "roboshop-monogdb-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, var.WORKSTATION_IP]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-mongodb-${var.ENV}"
+  }
+}
