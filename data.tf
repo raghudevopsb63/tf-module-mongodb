61073921ea8d4a129a65161ce7abc191d8ea9369
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-02 06:50:15 +0530

    Update terraform/tf-module-mongodb/

--- data.tf
+++ data.tf
@@ -11,6 +11,10 @@ data "aws_secretsmanager_secret" "secrets" {
   name = "${var.ENV}/roboshop/secrets"
 }
 
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
 output "data" {
-  value = data.aws_secretsmanager_secret.secrets
+  value = data.aws_secretsmanager_secret_version.secrets
 }
