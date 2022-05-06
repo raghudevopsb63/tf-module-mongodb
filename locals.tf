e75b431c4d88a991e5ed03c0a1bf197d8f8b558c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-06 06:13:46 +0530

    Update terraform/tf-module-mongodb/

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  DNS_NAME = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
+  DNS_NAME = aws_docdb_cluster.docdb.endpoint
   USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCUMENTDB_MASTER_USERNAME"]
   PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCUMENTDB_MASTER_PASSWORD"]
 }
